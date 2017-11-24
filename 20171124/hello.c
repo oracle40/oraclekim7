0c03c6bc7d39764380cb97a962c9c39169635624
oracle40 <oracle40@naver.com>
2017-11-24 13:00:23 +0900

    add hello.c

---
+++ 20171124/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// 첫번째 줄은 표준입출력파일을 포함하겠다는 의미
+
+// 다섯번째줄은 엔트리포인트(시작점)
+int main()
+{
+	/*헬로우월드 출력하기
+	\n은 newline의 축약임
+
+	\t는 tab만큼 띄워서 출력한다는 의미
+	 */
+	printf("Hello, World!\n");
+
+	return 0;
+}
+
+
+
+
